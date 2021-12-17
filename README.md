60605b060e553f7127ea753ab3cdb86c8ed72185
lim ho <dlagh123@gmail.com>
2021-12-17 14:40:27 +0900

    .

--- README.md
+++ README.md
@@ -7,17 +7,31 @@ $ npm init <br>
 $ npm install express --save<br>
 root 경로에 index.js 생성<br>
 
-## mongoDB
+### mongoDB
 https://cloud.mongodb.com/ <br>
 몽고DB 생성 <br>
 
-## mongoose 
+### mongoose 
 $ npm install mongoose --save<br>
+
+### body-parser
+$ npm install body-parser --save
+
+### postMan
+https://www.postman.com/
+
+### NODE MON
+소스변경시 서버 내렸다킬 필요없어짐 굿<br>
+$ npm install nodemon --save-dev <br>
+package.json script 에 <br>
+"backend": "nodemon index.js" 추가 <br>
+// backend 는 원하는대로 적어도됨
+
 
 ## git
 https://git-scm.com/ <br>
 
-#### 준비<br>
+#### init<br>
 $ git init<br>
 
 #### 상태보기<br>
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
 const mongoURL = 'mongodb+srv://limhoooo:dlagh12@template.fszzg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 const mongoose = require('mongoose')
+const { User } = require('./models/user');
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 mongoose.connect(mongoURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -12,5 +19,21 @@ mongoose.connect(mongoURL, {
     .then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err))
 
+
 app.get('/', (req, res) => res.send('hello World'));
+
+app.post('/register', (req, res) => {
+    // 회원가입 할때 필요한 정보들을 client 에서 가져오면
+    // 그것들을 데이터베이스에 넣어 준다.
+    const user = new User(req.body)
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
